0377cff3601c24f0f0e7e19c02ddff452ae22984
unknown <babihac@seznam.cz>
2021-06-04 11:29:04 +0200

    pridani komentu

--- pridej.php
+++ pridej.php
@@ -15,20 +15,31 @@ if (!$goods) {
     die("Neznámý produkt");
 }
 
-// $_SESSION['kosik'][] = $goods["produkt_id"];
+//promenna, ktera nakonec aktualizuje celkovou cenu v kosiku
 $priceUpdate = 0;
 $quantity = htmlspecialchars($_GET["quantity"]);
+// Situace, kdy zboží již existuje v košíku a došlo k přidání zboží z index.php
 if (isset($_SESSION['kosik'][$goods['produkt_id']]) && !isset($_GET['change'])) {
+    //promenna je nutna pro aktualizaci celkove cenz
     $old_quantity = intval($_SESSION['kosik'][$goods["produkt_id"]]['quantity']);
+    //aktualizace kusu produkut
     $_SESSION['kosik'][$goods["produkt_id"]] = ["quantity" => intval($quantity) + $old_quantity];
+    //od noveho poctu kusu odecteme stary pocet kusu => pricteme pocet kusu, ktere jsme pridali
     $priceUpdate = intval($_SESSION['kosik'][$goods["produkt_id"]]['quantity']) * intval($goods['cena'])
         - $old_quantity * intval($goods['cena']);
-} else if (isset($_SESSION['kosik'][$goods['produkt_id']]) && isset($_GET['change'])) {
+}
+
+//situace, kdy zbozi jiz existuje a  doslo k aktualizaci zbozi z kosik.php
+// change je hidden input, ktery slouzi k identifikaci situace zmeny zbozi
+else if (isset($_SESSION['kosik'][$goods['produkt_id']]) && isset($_GET['change'])) {
     $old_quantity = intval($_SESSION['kosik'][$goods["produkt_id"]]['quantity']);
     $_SESSION['kosik'][$goods["produkt_id"]] = ["quantity" => $quantity]; //přidání ID zboží do košíku
+    // stejny princip jako pri pridavani, ale tentokrat muze byt priceUpdate zaporna 
     $priceUpdate = intval($_SESSION['kosik'][$goods["produkt_id"]]['quantity']) * intval($goods['cena'])
         - $old_quantity * intval($goods['cena']);;
-} else {
+}
+//situace kdy pridaveme nove zbozi
+else {
     $_SESSION['kosik'][$goods["produkt_id"]] = ["quantity" => $quantity, "jednotkova_cena" => $goods['cena']];
     $priceUpdate = intval($_SESSION['kosik'][$goods["produkt_id"]]['quantity']) * intval($goods['cena']);
 }
