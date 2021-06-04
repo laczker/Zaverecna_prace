9afbc48a561c6d1bf2f7124e2a2ec1204314a4d2
unknown <babihac@seznam.cz>
2021-06-04 10:35:43 +0200

    pridani oauth

--- detail.php
+++ detail.php
@@ -1,62 +1,81 @@
 <?php
 require 'db.php';
 
-require 'overeni_uzivatele.php';
+
 
 $stmt = $db->prepare('SELECT * FROM produkt WHERE produkt.produkt_id=:produkt_id');
-$stmt->execute([':produkt_id'=>@$_REQUEST['produkt_id']]);
+$stmt->execute([':produkt_id' => @$_REQUEST['produkt_id']]);
 $goods = $stmt->fetch(PDO::FETCH_ASSOC);
 
 $produkt_id = $goods['produkt_id'];
-$nazev=$goods['nazev'];
-$popis=$goods['popis'];
-$cena=$goods['cena'];
-$kategorie_id=$goods['kategorie_id'];
-$vyrobce_id=$goods['vyrobce_id'];
-$parametry=$goods['parametry'];
+$nazev = $goods['nazev'];
+$popis = $goods['popis'];
+$cena = $goods['cena'];
+$kategorie_id = $goods['kategorie_id'];
+$vyrobce_id = $goods['vyrobce_id'];
+$parametry = $goods['parametry'];
 
 
-?><!DOCTYPE html>
+?>
+<!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="utf-8" />
     <title>Můj košík</title>
     <link rel="stylesheet" type="text/css" href="./styles.css">
 </head>
+
 <body>
 
-<?php
-if(empty($currentUser)){
-    include 'navbar_neprihlasen.php';
-}
-else{
-    include 'navbar.php';
-}
-?>
+    <?php
+    if (empty($currentUser)) {
+        include 'navbar_neprihlasen.php';
+    } else {
+        include 'navbar.php';
+    }
+    ?>
 
-<h1>Detail produktu</h1>
+    <h1>Detail produktu</h1>
 
 
-<a href="index.php">Zpět na výběr zboží</a>
+    <a href="index.php">Zpět na výběr zboží</a>
 
-<br/><br/>
+    <br /><br />
 
-<?php
-if (empty($goods)){
-    echo 'Nebyl vybrán žádný produkt k zobrazení detailu';}
-?>
+    <?php
+    if (empty($goods)) {
+        echo 'Nebyl vybrán žádný produkt k zobrazení detailu';
+    }
+    ?>
 
-<table>
-        <tr><td>Název: <?php echo htmlspecialchars(@$nazev);?></td></tr>
-        <tr><td>Popis: <?php echo htmlspecialchars(@$popis);?></td></tr>
-        <tr><td>Cena: <?php echo @$cena;?></td></tr>
-        <tr><td>Kategorie: <?php echo htmlspecialchars(@$kategorie_id);?></td></tr>
-        <tr><td>Výrobce: <?php echo htmlspecialchars(@$vyrobce_id);?></td></tr>
-        <tr><td>parametry: <?php echo htmlspecialchars(@$parametry);?></td></tr>
-    <? if (($currentUser['role']=='admin')): ?>
-        <tr><td><a href='uprav.php?produkt_id=<?php echo @$produkt_id; ?>'>Upravit</a></td></tr>
-        <tr><td><a href='odeber.php?produkt_id=<?php echo @$produkt_id; ?>'>Odstranit</a></td></tr>
-    <? endif; ?>
-    <table/>
+    <table>
+        <tr>
+            <td>Název: <?php echo htmlspecialchars(@$nazev); ?></td>
+        </tr>
+        <tr>
+            <td>Popis: <?php echo htmlspecialchars(@$popis); ?></td>
+        </tr>
+        <tr>
+            <td>Cena: <?php echo @$cena; ?></td>
+        </tr>
+        <tr>
+            <td>Kategorie: <?php echo htmlspecialchars(@$kategorie_id); ?></td>
+        </tr>
+        <tr>
+            <td>Výrobce: <?php echo htmlspecialchars(@$vyrobce_id); ?></td>
+        </tr>
+        <tr>
+            <td>parametry: <?php echo htmlspecialchars(@$parametry); ?></td>
+        </tr>
+        <? if (isset($currentUser) && ($currentUser['role'] == 'admin')) : ?>
+            <tr>
+                <td><a href='uprav.php?produkt_id=<?php echo @$produkt_id; ?>'>Upravit</a></td>
+            </tr>
+            <tr>
+                <td><a href='odeber.php?produkt_id=<?php echo @$produkt_id; ?>'>Odstranit</a></td>
+            </tr>
+        <? endif; ?>
+        <table />
 
 </html>
--- prihlaseni.php
+++ prihlaseni.php
@@ -3,52 +3,106 @@
 session_start();
 
 require 'db.php';
+require_once __DIR__ . "/vendor/autoload.php";
+$clientID = '131062190477-7n95rips3hpuro8nj4otggkdluqobo8r.apps.googleusercontent.com';
+$clientSecret = '13zGWs8ctd3nVe2PnvIVbHfJ';
+$redirectUri = 'http://localhost/Zaverecna_prace/prihlaseni.php';
 
-if (!empty($_POST)){
+$client = new Google_Client();
+$client->setClientId($clientID);
+$client->setClientSecret($clientSecret);
+$client->setRedirectUri($redirectUri);
+$client->addScope("email");
+$client->addScope("profile");
+
+
+
+if (isset($_GET['code'])) {
+    $token = $client->fetchAccessTokenWithAuthCode($_GET['code']);
+    $client->setAccessToken($token['access_token']);
+
+    // get profile info
+    $google_oauth = new Google_Service_Oauth2($client);
+    $google_account_info = $google_oauth->userinfo->get();
+    $email =  $google_account_info->email;
+    $name =  $google_account_info->name;
+
+    $stmt = $db->prepare("SELECT * FROM uzivatel WHERE email = ? LIMIT 1");
+    $stmt->execute([$email]);
+
+    if (($existingUser = $stmt->fetch(PDO::FETCH_ASSOC))) {
+        $_SESSION['uzivatel_id'] = $existingUser['uzivatel_id'];
+        header('Location: index.php');
+    } else {
+        $stmt = $db->prepare("INSERT INTO uzivatel (email, heslo, role) VALUES (?, ?, ?)");
+        $stmt->bindValue(1, $email);
+        $stmt->bindValue(2, "nemá heslo");
+        $stmt->bindValue(3, "user");
+        $stmt->execute();
+
+        $stmt = $db->prepare("SELECT * FROM uzivatel WHERE email = ? LIMIT 1");
+        $stmt->execute([$email]);
+        $existingUser = $stmt->fetch(PDO::FETCH_ASSOC);
+        $_SESSION['uzivatel_id'] = $existingUser['uzivatel_id'];
+        header('Location: index.php');
+    }
+
+
+    // now you can use this profile info to create account in your website and make user logged in.
+}
+
+if (!empty($_POST)) {
     $email = @$_POST['email'];
     $heslo = @$_POST['heslo'];
 
     $stmt = $db->prepare("SELECT * FROM uzivatel WHERE email = ? LIMIT 1");
     $stmt->execute([$email]);
 
-    if (($existingUser=$stmt->fetch(PDO::FETCH_ASSOC)) && password_verify($heslo, @$existingUser['heslo'])){
+    if (($existingUser = $stmt->fetch(PDO::FETCH_ASSOC)) && password_verify($heslo, @$existingUser['heslo'])) {
         $_SESSION['uzivatel_id'] = $existingUser['uzivatel_id'];
         header('Location: index.php');
-    }else{
-        $formError="Invalid user or password!";
+    } else {
+        $formError = "Invalid user or password!";
     }
-}?><!DOCTYPE html>
+} ?>
+<!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="utf-8" />
     <title>PHP Shopping App</title>
     <link rel="stylesheet" type="text/css" href="styles.css">
 </head>
+
 <body>
 
-<h1>PHP Shopping App</h1>
+    <h1>PHP Shopping App</h1>
 
-<h2>Sign in</h2>
+    <h2>Sign in</h2>
 
-<?php
-if (!empty($formError)){
-    echo '<p style="color:red;">'.$formError.'</p>';
-}
-?>
+    <?php
+    if (!empty($formError)) {
+        echo '<p style="color:red;">' . $formError . '</p>';
+    }
+    ?>
 
-<form method="post">
-    <label for="email">Váš email</label><br/>
-    <input type="text" name="email" id="email" value="<?php echo htmlspecialchars(@$_POST['email'])?>"><br/><br/>
+    <form method="post">
+        <label for="email">Váš email</label><br />
+        <input type="text" name="email" id="email" value="<?php echo htmlspecialchars(@$_POST['email']) ?>"><br /><br />
 
-    <label for="heslo">heslo</label><br/>
-    <input type="password" name="heslo" id="heslo" value=""><br/><br/>
+        <label for="heslo">heslo</label><br />
+        <input type="password" name="heslo" id="heslo" value=""><br /><br />
 
-    <input type="submit" value="Sign in">
-</form>
+        <input type="submit" value="Sign in">
+    </form>
 
-<br/>
+    <br />
 
-<a href="registrace.php">Registrovat se</a>
+
+    <a href='<?= $client->createAuthUrl() ?>'>Google Login</a>;
+
+    <a href="registrace.php">Registrovat se</a>
 
 </body>
+
 </html>
